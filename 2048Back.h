f28ba8e87e587c1cf3ed40a8d1f9f849b5b5aca6
Karahtar <nicoapaganini@gmail.com>
2015-10-22 00:23:46 -0300

    THIS IS HOW WE DO EET

--- 2048Back.h
+++ 2048Back.h
@@ -1,1 +1,29 @@
+#include <stdarg.h>
+
+#ifndef _backend2048_h
+
+#define _backend2048_h
+
+int tamMatriz(int dificultad);
+/* elige el tamaño de la matriz a partir de la dif*/
+
+int **creaMatriz(int dim);
+/* crea la matriz a partir del tamaño que le pases*/
+
+/*funciones de random vistas/hechas en clase*/
+double randNormalize(void);
+/* numero random entre 0 y 1 inclusive*/
+int randInt(int izq, int der);
+/* numero random entre el izq y el der usando la funcion randNormalize*/
+
+int calcAzar(void);
+/* calcula si ponerle un 2 o el raro caso del 4*/
+
+void nuevoNum(int dim, int **matriz);
+/* calcula donde ponerle el nuevo numero */
+
+void liberaMatriz(int i, int **matriz)
+/* si no hay espacio para jugar, libera la matriz antes de salir */
+
+#endif // _backend2048_h
 
