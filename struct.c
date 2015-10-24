6e8c531c88b95597236fcb247043cb957b0d63e8
Karahtar <nicoapaganini@gmail.com>
2015-10-24 17:41:49 -0300

    Flor?????????

--- struct.c
+++ struct.c
@@ -6,3 +6,19 @@ typedef? struct {
     int cantUndos;
     int score;
 }variable?;
+
+struct nombre {
+    int **matActual?;
+    int **matAux;
+    int cantUndos;
+    int score;
+};
+
+typedef? struct {
+    int **matActual?;
+    int **matAux;
+    int *cantUndos;
+    int *score;
+}variable?;
+
+
