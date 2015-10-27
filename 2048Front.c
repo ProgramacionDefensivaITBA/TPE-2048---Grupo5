264127133ae930dc612768277f77c0cfd0c57c0a
Karahtar <nicoapaganini@gmail.com>
2015-10-27 18:22:32 -0300

    Update 2048Front.c

--- 2048Front.c
+++ 2048Front.c
@@ -5,35 +5,53 @@
 #include <string.h>
 #include "backend2048.h"
 
+enum estado {MOVIMIENTO=1, UNDO, SAVE, QUIT, ERROR};
+
+void bienvenida(void);
 int menu(void);
 int juegoNuevo(int dificultad);
 void anunciaDific(int dificultad);
 void imprMatAct(int **matriz, int dim);
+int leerEntrada(char nombreArchivo[36], char *dirVec);
 
-/*
-typedef? struct {
-    int **matActual?;
+
+typedef struct {
+    int **matActual;
     int **matAux;
+    int **matMov;
     int cantUndos;
-    int score;
-}variable?;
+    int puntaje;
+    int dim;
+} jugadaT;
+/*
+typedef struct {
+    int **matActual;
+    int **matAux;
+    int **matMov;
+    int *cantUndos;
+    int *puntaje;
+    int dim;
+} juegoT;
+
+juegoT juego;
+
+verifMatriz(&jugada * pjugada;juego);
 */
 
 int
-main()                      // aca esta el main! jaja
+main()                      // aca esta el main
 {
-int dificultad=0, dim, juego=0, estado=0;
-int **matAux, **matActual;
-char * nombreArchivo[36], *dirVec;
-char s;
 srand(time(NULL));
 
-printf("    Bienvenido al juego 2048!\n");
-printf("\t(Version 1.0)\n");
-printf(" By Bortolozzi, Giorgi & Paganini\n");
-printf("\n\n\n");
+jugadaT pjugada;
+int dificultad=0, dim, juego=0, estado=0;
+char * nombreArchivo[36];
+char *dirVec;
+char s;
 
+bienvenida();
 juego=menu();
+
 switch(juego) {
     case 1: {
         putchar('\n');
@@ -42,17 +60,34 @@ switch(juego) {
         anunciaDific(dificultad);
         putchar('\n');
 
-        dim=tamMatriz(dificultad);
+        dim=pjugada.dim=tamMatriz(dificultad);
+
+        /*
         matAux=creaMatriz(dim);
-        if(matAux==NULL) {
+        (pjugada->matAux)=matAux;
+        */
+
+        pjugada.matAux=creaMatriz(pjugada.dim);
+        pjugada.matMov = creaMatriz(pjugada.dim);
+        pjugada.matActual = creaMatriz(pjugada.dim);
+
+        if(pjugada.matActual == NULL || pjugada.matAux==NULL || pjugada.matMov == NULL){
             printf("Oh oh!\nLo lamentamos pero no tiene suficiente espacio para jugar.\n\n");       // SALIMOS ASI NO MAS?
             return 0;
         }
+        /*
         matActual=creaMatriz(dim);
-        if(matActual==NULL) {
+        pjugada.matActual=creaMatriz(dim);
+        if(pjugada.matActual==NULL) {
             printf("Oh oh!\nLo lamentamos pero no tiene suficiente espacio para jugar.\n\n");       // SALIMOS ASI NO MAS?
             return 0;
         }
+        matMov=creaMatriz(dim);
+        pjugada.matMov=creaMatriz(dim);
+        if(pjugada.matMov==NULL) {
+            printf("Oh oh!\nLo lamentamos pero no tiene suficiente espacio para jugar.\n\n");
+            return 0;
+        }*/
         break;
     }
     case 2: {
@@ -68,26 +103,29 @@ switch(juego) {
 }
 
 /* agrego 2 random a la matriz */
-nuevoNum(dim, matActual);
-nuevoNum(dim, matActual);
+nuevoNum(dim, pjugada.matMov);
+nuevoNum(dim, pjugada.matMov);
+
+/* guardo la nueva en la actual */
+pjugada.matActual=memcpy(pjugada.matActual, pjugada.matMov, dim*sizeof(int));
 /* guardo la nueva en la aux */
-matAux=memcpy(matAux, matActual, dim*sizeof(int));
+pjugada.matAux=memcpy(pjugada.matAux, pjugada.matActual, dim*sizeof(int));
 /* la imprimo */
-imprMatAct(matActual, dim);
+imprMatAct(pjugada.matActual, dim);
 printf("\n\n\n");
 
-/* pido nuevo mov. Undo en 0 */
+/* pido nuevo mov. flagUndo en 0 */
 putchar('\n');
+
 do {
     estado=leerEntrada(nombreArchivo, &dirVec);
     switch(estado) {
         case 1: {
             /* hago el movimiento... si no es NULL, cambio matActual */
-            movimiento(&dirVec, dim, matActual);
+            movimiento(&dirVec, dim, pjugada.matActual);
             putchar('\n');
-            imprMatAct(matAux, dim);
 /*            if(verifMats(dim, matAux, matActual))     */
-                nuevoNum(dim, matActual);
+                nuevoNum(dim, pjugada.matActual);
 /*
 ** if(posibMov != Act)
 ** Aux=Act;     memcpy?
@@ -95,7 +133,7 @@ do {
 ** randNum-->Act;
 */
             putchar('\n');
-            imprMatAct(matActual, dim);
+            imprMatAct(pjugada.matActual, dim);
             printf("\n\n\n");
             putchar('\n');
             putchar('\n');
@@ -123,12 +161,13 @@ do {
             printf("\nComando no reconocido.\nLe recordamos que los comandos reconocidos son:\n");
             printf("w, a, s, d -- movimientos\nsave -- guardar\nundo -- volver un paso atras*\nquit -- salir de la partida al menu\n");
             printf("\n(Todos los comandos deben estar seguidos de un enter)\n");
-            printf("\n\n* El comando undo no permite hacer 2 undos seguido. ");
+            printf("\n\n* El comando undo no permite hacer 2 undos seguidos. ");
             printf("Adicionalmente, si hizo un movimiento que no efectuo cambios, el undo le devolvera la matriz anterior al ultimo cambio.\n\n");
             break;
         }
     }
-} while(leerEntrada(nombreArchivo, &dirVec)!=4);  // FALTARIA FIJARSE QUE NO HAYAN LLEGADO A 1024(facil) O A 2048
+
+} while(estado!=4);  // FALTARIA FIJARSE QUE NO HAYAN LLEGADO A 1024(facil) O A 2048
 
 // aux lo dejo igual por ahora
 /* probar usar un flagCambio que este en el movimiento que se prenda si hubo
@@ -137,6 +176,14 @@ do {
 */
 
 return 0;
+}
+
+void bienvenida(void)
+{
+printf("    Bienvenido al juego 2048!\n");
+printf("\t(Version 1.0)\n");
+printf(" By Bortolozzi, Giorgi & Paganini\n");
+printf("\n\n\n");
 }
 
 int
@@ -168,21 +215,110 @@ return dificultad;
 void
 anunciaDific(int dificultad)
 {
+printf("Ha seleccionado la dificultad ");
 switch(dificultad) {
     case 1: {
-        printf("Ha seleccionado la dificultad facil!\nLe deseamos suerte!\n");
+        printf("facil!\nLe deseamos suerte!\n");
         break;
     }
     case 2: {
-        printf("Ha seleccionado la dificultad intermedio.\nMucha suerte!\n");
+        printf("intermedio.\nMucha suerte!\n");
         break;
     }
     case 3: {
-        printf("Ha seleccionado la dificultad dificil!\nLe deseariamos suerte, pero en nuestra experiencia, no existe tal cosa.\n");
+        printf("dificil!\nLe deseariamos suerte, pero en nuestra experiencia, no existe tal cosa.\n");
         break;
     }
 }
 return;
+}
+
+
+int
+leerEntrada(char nombreArchivo[36], char *dirVec)
+{
+int turno, c;
+char * quit="uit\n";
+char * save="ve ";
+char * undo="ndo\n";
+int i, j, estado=0;
+
+turno=getchar();
+
+
+	    switch(turno) {
+			case 'w': {
+			    if(getchar()=='\n') {
+                    *dirVec=turno;
+                    estado=MOVIMIENTO;
+                    } else{
+                    estado=ERROR;
+			    }break;
+			}
+			case 'a': {
+			    if(getchar()=='\n') {
+                    *dirVec=turno;
+                    estado=MOVIMIENTO;
+			    } else
+                    estado=ERROR;
+			    break;
+			}
+			case 'd': {
+			    if(getchar()=='\n') {
+                    *dirVec=turno;
+                    estado=MOVIMIENTO;
+			    } else
+                    estado=ERROR;
+			    break;
+			}
+			case 'u': {
+			    for(i=0; i<4 && getchar() == undo[i] ; i++)
+               			;
+                	    if(i==4)
+		                    estado=UNDO;
+			    else
+		                    estado=ERROR;
+                	break;
+			}
+			case 's': {
+			    c=getchar();
+			    if(c=='\n') {
+                    *dirVec=turno;
+                    estado=MOVIMIENTO;
+			    } else if(c=='a') {
+			        for(i=0; i<3 && getchar() == save[i] ; i++)
+                        ;
+                    if(i==3) {
+                        estado=SAVE;
+                        j=0;
+                        do
+                            nombreArchivo[j] = getchar();
+                        while(j<35 && (nombreArchivo[j++]!='\n'));
+                        nombreArchivo[j]='\0';
+                        if(j==35)
+                            printf("\nEl nombre del archivo guardo solo los primeros 35 chars!!\n");
+                        } else
+                            estado=ERROR;
+                } else
+                    estado=ERROR;
+				break;
+			}
+			case 'q': {
+			    for(i=0; i<4 && getchar() == quit[i] ; i++)
+                       ;
+                if(i==4)
+                    estado=QUIT;
+                else
+                    estado=ERROR;
+			    break;
+			}
+			default: {
+			    while(getchar() != '\n');
+			    estado=ERROR;
+			}
+	    }
+
+return estado;
 }
 
 void
